35eded90d31db99efad5f39225cdf063ae5f958e
Able <Able@192.168.0.15>
2015-11-22 18:23:52 -0500

    Added extra commments

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 /*
-Program:Contains an 'Airline Ticket Reservation Application' and 'Dice Rolling Application'
+Name				: Able Varghese
+Student No			: 300 806 936
+Program				: Contains an 'Airline Ticket Reservation Application' and 'Dice Rolling Application'
+Program Description	: This class calls two application as you choose them.
+It contains an Airline Ticket Reservation Applicaiton and Dice Rolling Application
 
-Program Description: This class calls two application as you choose them.
-It contains an Airline ticket reservation Applicaiton and Dice Rolling Application
+******Please find individual 'program descriptions' for each applications in their respective classes*****
 
-Name: Able Varghese
-Student No: 300 806 936
  */
 
 import java.util.Random;
@@ -14,11 +15,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		//instantiating object for Scanner class
 		Scanner scan = new Scanner(System.in);
 
 		int select = 0;
 
+		//Menu for accessing applications
 		while (select != 3) {
 			System.out.println("\n*************************************************");
 			System.out.println(
@@ -29,13 +32,14 @@ public class Main {
 			if (select == 1) {
 				Dice_Rolling_App Obj1 = new Dice_Rolling_App();
 				select = 0;
-
 			}
 
 			else if (select == 2) {
 				Airline Flight = new Airline();
 				select = 0;
-			} else if (select == 3) {
+				}
+			
+			else if (select == 3) {
 				System.out.println("\nExiting the system......");
 				return;
 			}
