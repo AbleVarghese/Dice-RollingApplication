eb5a7dc6c1b6062c05be4eda1fa8ef7cc05663f0
Able <Able@192.168.0.15>
2015-11-22 15:08:12 -0500

    Part 2 of Assignment- Airline Reservation System
    Constructed Airline Class

---
+++ src/Airline.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Airline {
+
+	public Airline() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args){
+		
+		String input;
+		Scanner obj = new Scanner(System.in);
+		input=obj.next();
+		while(input!="3")
+		{
+			switch(input){
+			case "1":;
+				break;
+			case "2":;
+			break;
+			case "3":;
+			break;
+			default: ;
+				break;
+			}
+			
+		}
+		
+		
+		
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -7,55 +7,47 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-	
-		int ran1, ran2, ranSum;
-		int [] sumArray = new int[12]; 
-		int [] sumRepeat = new int[12]; 
-		
-		Random Dice1 = new Random();
-		Random Dice2= new Random();
-		
-		
-		//Initialize sumArray	
-		for(int i=0;i<11;i++){
-			
-			sumArray[i]=i+2;
-			
-			
-		}
-		
-		for(int j=0;j<11;j++){
-			
-			sumRepeat[j]=0;
-			
-			
-		}
-		
 
-		
-		for(int n=0;n<36000;n++){
-			
-			ran1 =1+Dice1.nextInt(6);
-			ran2 =1+Dice2.nextInt(6);
-			 ranSum = ran1+ran2;
-			 
-			 for(int m=0;m<35;m++){
-			 if(sumArray[m]==ranSum)
-			 {sumRepeat[m]++;
-			 break;			 }
-			 
-			 }
+		int ran1, ran2, ranSum;
+		int[] sumArray = new int[12];
+		int[] sumRepeat = new int[12];
+
+		Random Dice1 = new Random();
+		Random Dice2 = new Random();
+
+		// Initialize sumArray
+		for (int i = 0; i < 11; i++) {
+
+			sumArray[i] = i + 2;
+
 		}
-			 
-		
-		
-		//Print array
-		 
-			for(int k=0;k<11;k++)
-			{
-				System.out.println(sumArray[k]+"="+sumRepeat[k]);
+
+		for (int j = 0; j < 11; j++) {
+
+			sumRepeat[j] = 0;
+
+		}
+
+		for (int n = 0; n < 36000; n++) {
+
+			ran1 = 1 + Dice1.nextInt(6);
+			ran2 = 1 + Dice2.nextInt(6);
+			ranSum = ran1 + ran2;
+
+			for (int m = 0; m < 35; m++) {
+				if (sumArray[m] == ranSum) {
+					sumRepeat[m]++;
+					break;
+				}
+
 			}
-			
-	
+		}
+
+		// Print array
+
+		for (int k = 0; k < 11; k++) {
+			System.out.println("Total number of occurance of sum:"+sumArray[k] + " is = " + sumRepeat[k]);
+		}
+
 	}
 }
