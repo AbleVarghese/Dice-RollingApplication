0ad46321a8605fafc7e8a5fa6f34ce7debf4dd57
Able <Able@192.168.0.15>
2015-11-22 13:38:58 -0500

    Sum of two random strings calculated and displayed

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
+import java.util.Random;
 
 public class Main {
 
 	public Main() {
 		// TODO Auto-generated constructor stub
 	}
-System.Out.Println("jjkjhkjh");
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static void main(String[] args) {
+	
+		int ran1; int ran2;
+		
+		Random Dice1 = new Random();
+		Random Dice2 = new Random();
+		
+		
+		ran1 =1+Dice1.nextInt(6);
+		ran2 =1+Dice1.nextInt(6);
+		
+		System.out.println(ran1+ran2);
 	}
+	
 
 }
