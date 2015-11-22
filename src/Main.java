c83ab423ab36c8fe372ce952c0aac174059781a5
Able <Able@192.168.0.15>
2015-11-22 14:39:22 -0500

    Completed Dice Roll Application

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,54 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		int ran1; int ran2;
+		int ran1, ran2, ranSum;
+		int [] sumArray = new int[12]; 
+		int [] sumRepeat = new int[12]; 
 		
 		Random Dice1 = new Random();
-		Random Dice2 = new Random();
+		Random Dice2= new Random();
 		
 		
-		ran1 =1+Dice1.nextInt(6);
-		ran2 =1+Dice1.nextInt(6);
+		//Initialize sumArray	
+		for(int i=0;i<11;i++){
+			
+			sumArray[i]=i+2;
+			
+			
+		}
 		
-		System.out.println(ran1+ran2);
-	}
-	
+		for(int j=0;j<11;j++){
+			
+			sumRepeat[j]=0;
+			
+			
+		}
+		
 
+		
+		for(int n=0;n<36000;n++){
+			
+			ran1 =1+Dice1.nextInt(6);
+			ran2 =1+Dice2.nextInt(6);
+			 ranSum = ran1+ran2;
+			 
+			 for(int m=0;m<35;m++){
+			 if(sumArray[m]==ranSum)
+			 {sumRepeat[m]++;
+			 break;			 }
+			 
+			 }
+		}
+			 
+		
+		
+		//Print array
+		 
+			for(int k=0;k<11;k++)
+			{
+				System.out.println(sumArray[k]+"="+sumRepeat[k]);
+			}
+			
+	
+	}
 }
